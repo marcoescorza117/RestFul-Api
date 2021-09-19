3a494a9278922016c5de13b1c2bd84cdab1c8d3c
MarcoEscorza <marcoescorza117@gmail.com>
2021-09-18 19:49:03 -0500

    Agregando nuevo controlador y eliminando controlador de prueba

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetPost() {
+
+            return Ok(null);
+        
+        }
+    }
+}
