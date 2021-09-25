5729da2208172b999a4d5115d9fe8be0d1e47b33
MarcoEscorza <marcoescorza117@gmail.com>
2021-09-25 16:52:56 -0500

    Agregando Dtos - (Data Transfer Objects and automapper)

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
 using SocialMedia.Infrastructure.Repositories;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Api.Controllers
@@ -21,38 +23,76 @@ namespace SocialMedia.Api.Controllers
 
         }
 
+        //NO ACOPLAR A CONTROLADOR NUESTRO REPOSITORIO
+        //SE INCLUMPLE INYECCION DE DEPENDENCIAS
+
+        //Incorrecto, tiene que hacerse con inyeccion de dependencias
+        //var post = new PostRepository().GetPost();
+        //return Ok(post);
+        //
+
+        //Manera correcta de implementacion :) 
+        //Lo siguiente sirve para el scaffolding
+        //Scaffold-DbContext "Server=(localdb)\MSSQLLocalDB;Database=SocialMedia;Integrated Security = true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data
+
+
         [HttpGet]
         public async Task<IActionResult> GetPost() {
-            //NO ACOPLAR A CONTROLADOR NUESTRO REPOSITORIO
-            //SE INCLUMPLE INYECCION DE DEPENDENCIAS
-
-            //Incorrecto, tiene que hacerse con inyeccion de dependencias
-            //var post = new PostRepository().GetPost();
-            //return Ok(post);
-            //
-
-            //Manera correcta de implementacion :) 
-            //Lo siguiente sirve para el scaffolding
-            //Scaffold-DbContext "Server=(localdb)\MSSQLLocalDB;Database=SocialMedia;Integrated Security = true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data
+            
             var post = await _postRepository.GetPosts();
-            return Ok(post);
+
+            //Conviertiendo enumerable de entidad de dominio a entidad Dto
+            //Lambda expression
+            var postDTOs = post.Select(x => new PostDto
+            {
+                PostId = x.PostId,
+                Date = x.Date,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId
+
+            });
+            return Ok(postDTOs);
         
         }
 
-        //Obteneindoi un post
+        //Obteniendo un post
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
 
             var post = await _postRepository.GetPost(id);
-            return Ok(post);
+
+            var postDto = new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+                
+            };
+
+            return Ok(postDto);
 
         }
         
-        //Insertando un post
+        //Insertando un post -> DEJAR DE USAR ENTIDADES DE DOMINIO-... en este caso es al contrario
+        //de una entidad tipo dto pasarla a entiedad de tipo de dominio
         [HttpPost]
-        public async Task<IActionResult> Post(Post post)
+        public async Task<IActionResult> Post(PostDto postDto)
         {
+            //Mapeando entidad
+            var post = new Post
+            {
+
+                
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+
+            };
 
             await _postRepository.InsertPost(post);
             return Ok(post);
---
+++ SocialMedia.Core/DTOs/PostDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.DTOs
+{
+    //misma estructura de nuestra entidad de negocio que queremos emular
+    //los Dtos NO DEBEN DE TENER LOGICA EN ELLOS... SOLO SIRVEN PARA TRASMIUTIR INFORMACION
+    public class PostDto
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+    }
+}
