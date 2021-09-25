6c3e37040c869360385d5c5b9f7c76d109c3ec64
MarcoEscorza <marcoescorza117@gmail.com>
2021-09-25 17:00:21 -0500

    Instalando automapper dependency en SocialMedia.Api

---
+++ SocialMedia.Infrastructure/Mappings/AutomapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SocialMedia.Core.DTOs;
+using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Mappings
+{//registrar tipos de conversiones //herendado profile de automapper
+    public class AutomapperProfile :Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Post, PostDto>();
+            CreateMap<PostDto, Post>();
+        }
+    }
+}
